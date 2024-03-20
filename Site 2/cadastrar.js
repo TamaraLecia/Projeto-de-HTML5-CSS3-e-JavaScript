5e34dc726461bc0ff891f4b932eb72397353e05c
Tamara Lécia <139869944+TamaraLecia@users.noreply.github.com>
2024-03-20 09:08:34 -0300

    Commit projeto completo

---
+++ Site 2/cadastrar.js
@@ -0,0 +1,11 @@
+function cadastrar() {
+    document.getElementById("cadastro").innerHTML = "Compra realizada com sucesso";
+}
+
+function comprar(){
+    window.location = "comprar.html";
+}
+
+function voltar() {
+    window.location = "lanche.html";
+}
